407100d2ad94d705433f21c8e57e42f5f97775f9
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-03 20:42:48 +0200

    jaccard

--- jaccard.py
+++ jaccard.py
@@ -1,5 +1,31 @@
 import os
 import glob
+from PIL import Image, ImageDraw as D
+import torchvision.transforms as transforms
+import torchvision
+import torch
+import numpy as np
+
+def create_boolean_mask(mask_img):
+    mask_img = F.rgb_to_grayscale(mask_img)
+    m_shape = mask_img.shape
+    if len(m_shape) == 3:
+        mask_img=mask_img.squeeze()
+    else:
+        mask_img=mask_img[:, 0, :, :]
+    mask = mask_img.numpy()
+    if mask.max() - mask.min() < 0.0001:
+        return torch.zeros(mask_img.shape).bool()
+
+    mask = (mask - mask.min()) / (mask.max() - mask.min())
+    mask = (mask * 255).astype(np.uint8)
+    #max_val = np.max(mask)
+    #min_val = np.min(mask)
+    #max_mask = (mask == max_val)
+    #min_mask = (mask == min_val)
+    bool_mask = mask > 100
+    return torch.tensor(bool_mask)
+
 
 # Define the root directory where you want to search
 root_dir = "/data/pwojcik/ProtoPShare/saved_models/resnet18/003/16push0.8675_nearest_train/"
@@ -16,3 +42,6 @@ filtered_files = [file_path for file_path in files if not os.path.basename(file_
 # Print the filtered list of files
 for file_path in filtered_files:
     print(file_path)
+    mask = Image.open(file_path).convert("RGB")
+    msk_tensor = transforms.ToTensor()(mask)
+    bool_mask = create_boolean_mask(msk_tensor)
