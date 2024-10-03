bd309b41c2fa43de5f56d1ebe2f3a96c462e753b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-03 20:48:18 +0200

    jaccard

--- jaccard.py
+++ jaccard.py
@@ -40,6 +40,8 @@ files = glob.glob(pattern, recursive=True)
 # Filter out files that match the "nearest-6_" pattern
 filtered_files = [file_path for file_path in files if not os.path.basename(file_path).startswith('nearest-6_')]
 
+jaccard = []
+
 # Print the filtered list of files
 for file_path in filtered_files:
     print(file_path)
@@ -47,4 +49,7 @@ for file_path in filtered_files:
     msk_tensor = transforms.ToTensor()(mask)
     bool_mask = create_boolean_mask(msk_tensor)
     num_white_pixels = torch.sum(bool_mask).item()
-    print(num_white_pixels/(bool_mask.shape[0] * bool_mask.shape[1]))
+    jaccard.append(num_white_pixels/(bool_mask.shape[0] * bool_mask.shape[1]))
+
+import statistics
+print('Jaccard: ', statistics.mean(jaccard))
